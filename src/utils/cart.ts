950000d033b3f06aa157e4f268790bebccf35f96
dineshsinghchouhan462-png <dineshsinghchouhan462@gmail.com>
2026-03-11 20:16:21 +0530

    Update cart.ts

--- src/utils/cart.ts
+++ src/utils/cart.ts
@@ -1,1 +1,24 @@
 
+export const getCart = () => {
+  const cart = localStorage.getItem("cart");
+  return cart ? JSON.parse(cart) : [];
+};
+
+export const saveCart = (cart: any[]) => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
+
+export const addToCart = (item: any) => {
+  const cart = getCart();
+  cart.push(item);
+  saveCart(cart);
+};
+
+export const removeFromCart = (slug: string) => {
+  const cart = getCart().filter((item: any) => item.slug !== slug);
+  saveCart(cart);
+};
+
+export const clearCart = () => {
+  localStorage.removeItem("cart");
+};
