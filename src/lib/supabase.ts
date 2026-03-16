2a9de168b0bdf3af5182f13cdb9bce0a134fc4c2
dineshsinghchouhan462-png <dineshsinghchouhan462@gmail.com>
2026-03-16 18:24:45 +0530

    Create supabase.ts

---
+++ src/lib/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://sjdbqfcoewdgllzkawfz.supabase.co";
+
+const supabaseKey = "sb_publishable_dLvHOHbs81qBOxE36RXeLg_6543yFY6";
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
