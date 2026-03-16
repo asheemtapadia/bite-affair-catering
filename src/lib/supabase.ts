a9d4b52547f883330a55e72991f1b625810ebb16
dineshsinghchouhan462-png <dineshsinghchouhan462@gmail.com>
2026-03-16 19:44:24 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,14 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://sjdbqfcoewdgllzkawfz.supabase.co";
-const supabaseKey = "sb_publishable_dLvHOHbs81qBOxE36RXeLg_6543yFY6";
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey,
-  {
-    global: {
-      fetch: (...args) => fetch(...args)
-    }
+const supabaseKey =
+  "sb_publishable_dLvHOHbs81qBOxE36RXeLg_6543yFY6";
+
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: false
   }
-);
+});
