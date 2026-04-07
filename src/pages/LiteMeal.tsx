ddda3258b3b7f6a51207977bb758b39834653bf1
asheemtapadia <asheemtapadia@gmail.com>
2026-04-07 21:02:41 +0530

    Update LiteMeal.tsx

--- src/pages/LiteMeal.tsx
+++ src/pages/LiteMeal.tsx
@@ -53,7 +53,7 @@ const LiteMeal = () => {
   const [time, setTime] = useState("");
 
   const [error, setError] = useState("");
-
+const [remark, setRemark] = useState("");
   const timeSlots = [
     "9:00 AM","10:00 AM","11:00 AM","12:00 PM",
     "1:00 PM","2:00 PM","3:00 PM","4:00 PM",
